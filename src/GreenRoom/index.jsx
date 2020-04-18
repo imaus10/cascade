50a6835b0f3492673b88e696e55282975a4c306a
Austin Blanton <imaus10@gmail.com>
2020-04-18 18:41:43 -0400

    Display connection string

--- src/GreenRoom/index.jsx
+++ src/GreenRoom/index.jsx
@@ -1,14 +1,29 @@
 import React, { useState } from 'react';
+import Peer from 'simple-peer';
 import VideoSquare from '../VideoSquare';
 
 const GreenRoom = () => {
     const [stream, setStream] = useState(null);
+    const [connectionString, setConnectionString] = useState(null);
+
+    const afterPermission = (stream) => {
+        setStream(stream);
+        const me = new Peer({
+            initiator: true,
+            stream
+        });
+        // Provides the connection string
+        me.on('signal', (connectionInfo) => {
+            setConnectionString(JSON.stringify(connectionInfo));
+        });
+    };
+
     const getPermission = () => {
         // Ask for webcam/microphone permission
         navigator.mediaDevices.getUserMedia({
             audio : true,
             video : true
-        }).then(setStream);
+        }).then(afterPermission);
     };
 
     return <>
@@ -16,7 +31,15 @@ const GreenRoom = () => {
         <div>First, let's enable your audio and video. So make yourself presentable. Put some pants on (or take them off if it's that kinda thing). Put on your makeup and/or mask. Enable your autotune whatnots.</div>
         { stream ?
             <VideoSquare stream={stream} /> :
-            <button onClick={getPermission}>Let's go!</button> }
+            <button onClick={getPermission}>Let's go!</button>
+        }
+        { connectionString &&
+            <>
+                {console.log(connectionString)}
+                <div>Your connection string:</div>
+                <textarea readOnly value={connectionString} />
+            </>
+        }
     </>;
 };
 
