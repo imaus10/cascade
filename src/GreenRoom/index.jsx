b7a922c8b51757394d58e4ce24aea18af204c50e
Austin Blanton <imaus10@gmail.com>
2020-04-18 18:06:25 -0400

    Add button to start webcam

--- src/GreenRoom/index.jsx
+++ src/GreenRoom/index.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import VideoSquare from '../VideoSquare';
 
 const GreenRoom = () => {
     const [stream, setStream] = useState(null);
-    useEffect(() => {
+    const getPermission = () => {
         // Ask for webcam/microphone permission
         navigator.mediaDevices.getUserMedia({
             audio : true,
             video : true
         }).then(setStream);
-    }, []);
+    };
 
     return <>
         <div>Welcome. Let's make the connections.</div>
         <div>First, let's enable your audio and video. So make yourself presentable. Put some pants on (or take them off if it's that kinda thing). Put on your makeup and/or mask. Enable your autotune whatnots.</div>
-        <VideoSquare stream={stream} />
+        { stream ?
+            <VideoSquare stream={stream} /> :
+            <button onClick={getPermission}>Let's go!</button> }
     </>;
 };
 
