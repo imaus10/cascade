921679964953a63a287033e3a1f218735b9feb0a
Austin Blanton <imaus10@gmail.com>
2020-04-19 11:41:00 -0400

    Send text between peers

--- src/GreenRoom/index.jsx
+++ src/GreenRoom/index.jsx
@@ -3,19 +3,34 @@ import Peer from 'simple-peer';
 import VideoSquare from '../VideoSquare';
 
 const GreenRoom = () => {
+    const [other, setOther] = useState(null);
+    const [myConnectionString, setMyConnectionString] = useState('');
+    const [otherConnectionString, setOtherConnectionString] = useState('');
     const [stream, setStream] = useState(null);
-    const [connectionString, setConnectionString] = useState(null);
+    const initiator = window.location.hash === '#init';
 
     const afterPermission = (stream) => {
         setStream(stream);
-        const me = new Peer({
-            initiator: true,
-            stream
+        const peer = new Peer({
+            initiator,
+            stream,
+            // Set to false for now, because with true it sends multiple signals
+            trickle: false
         });
         // Provides the connection string
-        me.on('signal', (connectionInfo) => {
-            setConnectionString(JSON.stringify(connectionInfo));
+        peer.on('signal', (connectionInfo) => {
+            setMyConnectionString(JSON.stringify(connectionInfo));
         });
+        // When the remote peer is connected
+        peer.on('connect', () => {
+            console.log('we connected!');
+            peer.send('HELLO WORLD');
+        });
+        // When receiving data from the remote peer
+        peer.on('data', (data) => {
+            console.log('received data: ', data.toString());
+        });
+        setOther(peer);
     };
 
     const getPermission = () => {
@@ -26,6 +41,11 @@ const GreenRoom = () => {
         }).then(afterPermission);
     };
 
+    const connectToOther = (event) => {
+        event.preventDefault();
+        other.signal(JSON.parse(otherConnectionString));
+    };
+
     return <>
         <div>Welcome. Let's make the connections.</div>
         <div>First, let's enable your audio and video. So make yourself presentable. Put some pants on (or take them off if it's that kinda thing). Put on your makeup and/or mask. Enable your autotune whatnots.</div>
@@ -33,12 +53,18 @@ const GreenRoom = () => {
             <VideoSquare stream={stream} /> :
             <button onClick={getPermission}>Let's go!</button>
         }
-        { connectionString &&
-            <>
-                {console.log(connectionString)}
-                <div>Your connection string:</div>
-                <textarea readOnly value={connectionString} />
-            </>
+        { stream &&
+            <form onSubmit={connectToOther}>
+                <label>
+                    Your connection info:
+                    <textarea readOnly value={myConnectionString} />
+                </label>
+                <label>
+                    Their connection info:
+                    <textarea onChange={(event) => setOtherConnectionString(event.target.value)} />
+                </label>
+                <input type="submit" value="Connect" />
+            </form>
         }
     </>;
 };
