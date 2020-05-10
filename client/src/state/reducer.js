1f6745d632522172f0e59d16c46b5423faebfa64
Austin Blanton <imaus10@gmail.com>
2020-05-10 18:28:31 -0400

    Move server and peer logic to separate file

--- client/src/state/reducer.js
+++ client/src/state/reducer.js
@@ -1,4 +1,4 @@
-import Peer from 'simple-peer';
+import { handleServerMessage, sendToServer } from './server-actions';
 
 export const initialState = {
     // The initiator is whoever is in the first spot.
@@ -30,76 +30,6 @@ function replacePeerStreams(state, action) {
         peer.removeStream(oldStream)
         peer.addStream(newStream);
     });
-}
-
-function handleServerMessage(state, action) {
-    const { myStream, peers, server } = state;
-    // Sending dispatch in the action is kind of a hack...
-    const { data, dispatch } = action;
-    const {
-        forId  : myId,
-        fromId : theirId,
-        order,
-        signal : receiveSignal,
-        type
-    } = JSON.parse(data);
-    if (type === 'id') {
-        dispatch({
-            type : 'MY_ID_SET',
-            id   : myId,
-        });
-    } else if (type === 'order') {
-        dispatch({
-            type : 'ORDER_SET',
-            order
-        });
-    } else if (type === 'signal') {
-        const initiator = receiveSignal === 'initiate';
-        const isNewPeer = !Object.keys(peers).includes(theirId);
-        const peer = isNewPeer ?
-            new Peer({
-                initiator,
-                stream : myStream,
-            }) :
-            peers[theirId];
-        if (isNewPeer) {
-            peer.on('signal', (signal) => {
-                sendToServer(server, {
-                    type   : 'signal',
-                    forId  : theirId,
-                    fromId : myId,
-                    signal
-                });
-            });
-            peer.on('stream', (theirStream) => {
-                dispatch({
-                    type   : 'STREAMS_ADD',
-                    id     : theirId,
-                    stream : theirStream
-                });
-            });
-        }
-
-        if (!initiator) {
-            peer.signal(receiveSignal);
-        }
-
-        if (isNewPeer) {
-            return {
-                ...state,
-                peers : {
-                    ...peers,
-                    [theirId] : peer
-                }
-            };
-        }
-    }
-
-    return state;
-}
-
-function sendToServer(server, data) {
-    server.send(JSON.stringify(data));
 }
 
 // This reducer is not quite a pure function and I'm not sorry about it.
