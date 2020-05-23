46ca3fcace1c394b258098af9406879cda862fce
Austin Blanton <imaus10@gmail.com>
2020-05-23 13:09:47 -0400

    Updates

--- precache-manifest.a698bb979f8b60e36e64689508367d03.js
+++ precache-manifest.a698bb979f8b60e36e64689508367d03.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0fa4ef77c6cfbfa7a5f76ce6dd79e41",
+    "revision": "4e066febb0e375c53ea8c746ca82e81c",
     "url": "/cascade/index.html"
   },
   {
-    "revision": "e258de1eb51205e76f60",
+    "revision": "b19e684c81c8e0355652",
     "url": "/cascade/static/css/main.6242fb5b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cascade/static/js/2.83f9b625.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e258de1eb51205e76f60",
-    "url": "/cascade/static/js/main.6a49a323.chunk.js"
+    "revision": "b19e684c81c8e0355652",
+    "url": "/cascade/static/js/main.dae147b0.chunk.js"
   },
   {
     "revision": "803ebf35026f8828ebf1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cascade/precache-manifest.86ac1fc9eebebdb2c06f7e6105426098.js"
+  "/cascade/precache-manifest.a698bb979f8b60e36e64689508367d03.js"
 );
 
 self.addEventListener('message', (event) => {
