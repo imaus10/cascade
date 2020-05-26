2115ac56949257e34dfb3a198fd0615f57854592
Austin Blanton <imaus10@gmail.com>
2020-05-26 16:06:41 -0400

    Separate Navigation component

--- client/src/components/GreenRoom.jsx
+++ client/src/components/GreenRoom.jsx
@@ -1,18 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { Context } from './Store';
+import Navigation from './Navigation';
 import VideoGrid from './VideoGrid';
 import Welcome from './Welcome';
-import {
-    CASCADE_DONE,
-    CASCADE_RECORDING,
-    READY,
-    startCascade,
-    stopCascade
-} from '../state/actions/cascade';
 
 const GreenRoom = () => {
-    const [state, dispatch] = useContext(Context);
-    const { files, iAmInitiator, mode, order } = state;
+    const [state] = useContext(Context);
+    const { files } = state;
     const [showWelcome, setShowWelcome] = useState(true);
 
     if (showWelcome) {
@@ -21,22 +15,7 @@ const GreenRoom = () => {
 
     return <>
         <VideoGrid />
-        <nav>
-            { [READY, CASCADE_DONE].includes(mode) && iAmInitiator && order.length > 1 &&
-                <button
-                    className="big-button"
-                    onClick={() => startCascade(dispatch)}
-                >
-                    GO
-                </button> }
-            { mode === CASCADE_RECORDING && iAmInitiator &&
-                <button
-                    className="big-button"
-                    onClick={() => stopCascade(dispatch)}
-                >
-                    STOP
-                </button> }
-        </nav>
+        <Navigation />
         <aside>
             { files.map((blobURL, index) =>
                 <a
---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Context } from './Store';
+import {
+    CASCADE_DONE,
+    CASCADE_RECORDING,
+    READY,
+    startCascade,
+    stopCascade
+} from '../state/actions/cascade';
+
+const Navigation = () => {
+    const [state, dispatch] = useContext(Context);
+    const { iAmInitiator, mode, order } = state;
+    return (
+        <nav>
+            { [READY, CASCADE_DONE].includes(mode) && iAmInitiator && order.length > 1 &&
+                <button
+                    className="big-button"
+                    onClick={() => startCascade(dispatch)}
+                >
+                    GO
+                </button> }
+            { mode === CASCADE_RECORDING && iAmInitiator &&
+                <button
+                    className="big-button"
+                    onClick={() => stopCascade(dispatch)}
+                >
+                    STOP
+                </button> }
+        </nav>
+    );
+};
+
+export default Navigation;
